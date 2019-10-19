9cbe5893017e00afa94924dadbc0e04a5f196591
Sahar <hello@sahar.pk>
2019-10-20 01:01:08 +1000

    Update installConky.sh

--- installConky.sh
+++ installConky.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+echo "Update System"
 sudo apt-get update -y
+echo "Installing Conky"
 sudo apt-get install -y conky-all
+echo "Downloading conky configuration file"
 sudo wget -O /home/pi/.conkyrc https://raw.githubusercontent.com/msanaullahsahar/rpi-conky/master/.conkyrc
 sudo chmod a+x /home/pi/.conkyrc
+echo "Downloading conky autostart file"
 sudo wget https://raw.githubusercontent.com/msanaullahsahar/rpi-conky/master/conky.sh
 sudo mv conky.sh /usr/bin/
+echo "Downloading desktop startup entry for conky"
 sudo wget https://raw.githubusercontent.com/msanaullahsahar/rpi-conky/master/conky.desktop
 sudo mv conky.desktop /etc/xdg/autostart/
+echo "Rebooting RaspberryPi"
 sudo reboot
 
